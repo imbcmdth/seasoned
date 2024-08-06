60317d2fcd4ae088d08495a4296505282d106511
Jon-Carlos Rivera <jon.carlos.rivera@gmail.com>
2024-08-06 13:00:38 -0700

    Make columns completely configurable and added a help

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+#!/usr/bin/env node
 const fs = require('fs');
 const path = require('path');
 
+// These are the meta columns and will appear first the CSV
+const metaColumns = [
+    // The format is:
+    // ['Column Name', 'fileMeta Field']
+    ['File Name', 'fileName'],
+    ['Row Number', 'rowNumber'],
+    ['Raw Line', 'rawLine'],
+    ['Participant Id', 'participantId'],
+    ['Year', 'year'],
+    ['Language', 'language'],
+    ['Coder', 'coder'],
+];
+
+// These are the Codes that we look for and output as individual columns.
+// To add new codes, remove codes, or change the order of columns simply update the
+// list here:
 const codeColumns = [
     '[SI-X]',
     '[SI-1]',
@@ -28,11 +45,11 @@ const codeColumns = [
     '[IDK]',
 ];
 
+// Create a set of upperCase column names to use for searching
 const codeColumnsUpperCase = codeColumns.map(c => c.toUpperCase());
 
-const protoResules = codeColumns.slice(0).map(() => 0); // Create a set of 0 columns that is the same length as `columns`
-const fileNames = process.argv.slice(2).map((p) => path.resolve(p));
-const outputRows = [];
+// Create a set of 0 columns that is the same length as `columns`
+const protoResults = codeColumns.slice(0).map(() => 0);
 
 const parseFileName = (absoluteFilePath) => {
     const extension = path.extname(absoluteFilePath);
@@ -40,7 +57,7 @@ const parseFileName = (absoluteFilePath) => {
     const parts = justTheName.toUpperCase().split('_');
 
     return {
-        name: justTheName,
+        fileName: justTheName,
         participantId: parts[0],
         year: parts[1],
         language: parts[2],
@@ -49,17 +66,31 @@ const parseFileName = (absoluteFilePath) => {
     }
 };
 
+const createLineMeta = (fileMeta, lineNum, line) => {
+    const lineMeta = Object.create(fileMeta);
+    lineMeta.rowNumber = lineNum;
+    lineMeta.rawLine = line;
+
+    return lineMeta;
+};
+
+const isChildLine = (line) => line.toLowerCase().trim().startsWith('c ');
+
 const getChildLinesFromFile = (absoluteFilePath) => {
     const data = fs.readFileSync(absoluteFilePath).toString();
-    const lines = data.split(/\r?\n/);
-    const onlyChildLines = lines.filter((line) => line.toLowerCase().trim().startsWith('c '));
+
+    // Replace any "Narrow No-Break Space" characters with a regular space
+    const cleanData = data.replace(/\u202f/g, ' ');
+
+    const lines = cleanData.split(/\r?\n/);
+    const onlyChildLines = lines.filter(isChildLine);
 
     return onlyChildLines;
 };
 
 const countCodes = (line) => {
     const codesMatch = line.toUpperCase().match(/(\[[^\]]+\])/g);
-    const results = protoResules.slice();
+    const results = protoResults.slice();
 
     if (!codesMatch) {
         return results;
@@ -76,15 +107,18 @@ const countCodes = (line) => {
 };
 
 const writeHeader = () => {
-    process.stdout.write(`"Filename", "Row Number", "Raw Line", "Participant Id", "Year", "Language", "Coder", `);
+    // Write the metaColumn part of the header
+    metaColumns.forEach((a) => process.stdout.write(`"${a[0]}", `));
+
+    // Write the codeColumn part of the header
     const columnsNotLast = codeColumns.slice(0);
     const columnLast = columnsNotLast.pop();
-    columnsNotLast.forEach(c => process.stdout.write(`"${c}", `));
+    columnsNotLast.forEach((c) => process.stdout.write(`"${c}", `));
     process.stdout.write(`"${columnLast}"\n`);
 };
 
-const writeLine = (fileMeta, line, rowNumber, codeCounts) => {
-    process.stdout.write(`"${fileMeta.name}", ${rowNumber}, "${line}", "${fileMeta.participantId}", "${fileMeta.year}", "${fileMeta.language}", "${fileMeta.coder}", `);
+const writeLine = (lineMeta, codeCounts) => {
+    metaColumns.forEach((a) => process.stdout.write(`"${lineMeta[a[1]]}", `));
     process.stdout.write(codeCounts.join(', ') + '\n');
 };
 
@@ -93,12 +127,39 @@ const buildCSV = (listOfFiles) => {
 
     listOfFiles.forEach((fileName) => {
         const fileMeta = parseFileName(fileName);
+
+        if (!fileMeta.isFinal) {
+            console.warn(`WARNING: Excluding non-FINAL file found in input set: ${fileName}`);
+            return;
+        }
+
         const lines = getChildLinesFromFile(fileName);
         lines.forEach((line, rowNumber) => {
             const lineCodes = countCodes(line);
-            writeLine(fileMeta, line, rowNumber, lineCodes);
+            const lineMeta = createLineMeta(fileMeta, rowNumber, line);
+            writeLine(lineMeta, lineCodes);
         });
     });
 };
 
+const printHelp = () => {
+    const wasStartedAsBin = process.argv[1].endsWith('seasoning');
+    const commandLine = wasStartedAsBin ? path.basename(process.argv[1]) : 'node ' + path.relative(process.cwd(), process.argv[1]);
+
+    console.error('ERROR: No files in the argument list.');
+    console.error();
+    console.error('To use this command, pass in a wildcard, or a list of files and save the output to a csv file.');
+    console.error();
+    console.error('For example, to run the command with two explicitly named files:');
+    console.error(`${commandLine}  12345678_Y1_ENG_FINAL.slt 12345678_Y1_ENG_FINAL.slt > output.csv`);
+    console.error();
+    console.error('For example, to run the command with ALL files in C:\\My Salt Files Directory\\ with the extension ".slt":');
+    console.error(`${commandLine}  "C:\\My Salt Files Directory\\*.slt" > output.csv`);
+};
+
+if (process.argv.length < 3 || process.argv[2] === '/?' || process.argv[2] === '--help') {
+    return printHelp();
+}
+
+const fileNames = process.argv.slice(2).map((p) => path.resolve(p));
 buildCSV(fileNames);
